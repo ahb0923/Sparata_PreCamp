9505bf7b47f1e7f4a5be5bc6c56f0e010352322a
Hyobin <37098177+ahb0923@users.noreply.github.com>
2025-03-18 17:40:53 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿for(int i=1; i<=100; i++)
+{
+    if (i % 2 != 0)
+    {
+        Console.WriteLine(i);
+    }
+}
+
+int j = 1;
+while (j <= 100)
+{
+    if (j % 2 != 0)
+    {
+        Console.WriteLine(j);
+    }
+    j++;
+}
+
+int k = 0;
+do
+{
+    k++;
+    if (k % 2 != 0)
+    {
+        Console.WriteLine(k);
+    }
+} while (k < 100);
